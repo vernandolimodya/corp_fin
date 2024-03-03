69cf93b2ae812dcf8ade0f8a08045ae5dd8129bc
Vernando Limodya <141973840+vernandolimodya@users.noreply.github.com>
2024-03-03 18:09:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class program:
+
+    def __init__(self):
+        welcome_text = """
+        -------------------------------
+        CORPORATE FINANCE
+        by Limodya, Vernando Fransiscus
+        created February 17th, 2024
+        -------------------------------
+        """
+        print(welcome_text)
+
+if __name__ == '__main__':
+    start = program()
